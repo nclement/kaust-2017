d39a2bd966d7ed28cfaf744a2e1eea7ab136eb12
Nathan Clement <nclement@cs.utexas.edu>
2017-01-26 10:04:53 -0600

    Egg just started

---
+++ 26Jan/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+void cupPlate() {
+    // Draw the cup and plate
+    cout << "\\        /" << endl;
+    cout << " \\______/" << endl;
+    cout << "+--------+" << endl;
+    cout << "\n";
+}
+
+void cap() {
+    // Draw the cap
+    cout << "  ++++++" << endl;
+    cout << " /      \\" << endl;
+    cout << "/        \\" << endl;
+    cout << "\n";
+}
+
+int main() {
+    cap();
+
+    // Draw the cup and plate
+    cout << "\\        /" << endl;
+    cout << " \\______/" << endl;
+    cout << "+--------+" << endl;
+    cout << "\n";
+    // Draw the Stop Sign
+    cout << "  ++++++" << endl;
+    cout << " /      \\" << endl;
+    cout << "/        \\" << endl;
+    cout << "|  STOP  |" << endl;
+    cout << "\\        /" << endl;
+    cout << " \\______/" << endl;
+    cout << "\n";
+    // Draw the hat
+    cout << "  ++++++" << endl;
+    cout << " /      \\" << endl;
+    cout << "/        \\" << endl;
+    cout << "+--------+" << endl;
+    return 0;
+}
