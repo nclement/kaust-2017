8e7b297bd9280ffec18e30fa278dcfdfefe731ec
Nathan Clement <nclement@cs.utexas.edu>
2017-01-26 15:20:18 -0600

    after class

--- 26Jan/main.cpp
+++ 26Jan/main.cpp
@@ -2,42 +2,106 @@
 
 using namespace std;
 
-void cupPlate() {
-    // Draw the cup and plate
+void top2bottom() {
+
+}
+
+void _this_() {
+
+}
+
+/**
+ * bad
+ *
+void 2hot2handle() {
+
+}
+ */
+
+void top() {
+    cout << "  ______" << endl;
+    cout << " /      \\" << endl;
+    cout << "/        \\" << endl;
+}
+
+void bottom() {
     cout << "\\        /" << endl;
     cout << " \\______/" << endl;
+}
+
+void hat() {
+    // Draw the hat
+    top();
+    cout << "+--------+" << endl;
+}
+
+void stopSign() {
+    // Draw the Stop Sign
+    top();
+    cout << "|  STOP  |" << endl;
+    bottom();
+
+    cout << "\n";
+}
+
+void cupPlate() {
+    // Draw the cup and plate
+    bottom();
     cout << "+--------+" << endl;
     cout << "\n";
 }
 
 void cap() {
     // Draw the cap
-    cout << "  ++++++" << endl;
-    cout << " /      \\" << endl;
-    cout << "/        \\" << endl;
+    top();
     cout << "\n";
+}
+
+void say10() {
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+}
+
+void say20() {
+    say10();
+    say10();
 }
 
 int main() {
     cap();
 
-    // Draw the cup and plate
-    cout << "\\        /" << endl;
-    cout << " \\______/" << endl;
-    cout << "+--------+" << endl;
-    cout << "\n";
-    // Draw the Stop Sign
-    cout << "  ++++++" << endl;
-    cout << " /      \\" << endl;
-    cout << "/        \\" << endl;
-    cout << "|  STOP  |" << endl;
-    cout << "\\        /" << endl;
-    cout << " \\______/" << endl;
-    cout << "\n";
-    // Draw the hat
-    cout << "  ++++++" << endl;
-    cout << " /      \\" << endl;
-    cout << "/        \\" << endl;
-    cout << "+--------+" << endl;
+    cupPlate();
+
+    stopSign();
+
+    hat();
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+    cout << "All work and no play makes Jack a dull boy" << endl;
+
     return 0;
 }
