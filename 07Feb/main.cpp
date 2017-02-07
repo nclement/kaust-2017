8463f90bb7be92dda699deabe444842aef20e77e
Nathan Clement <nclement@cs.utexas.edu>
2017-02-07 11:07:10 -0600

    After class 07 Feb

--- 07Feb/main.cpp
+++ 07Feb/main.cpp
@@ -6,19 +6,20 @@ void printPeopleArea() {
     int small, medium, large, people;
     double price, price2, area, area2;
     int people2 = 1;
-    // Run the following code for 1 <= people2 <= 100
-    people = people2;
-    large = people / 7;
-    people = people % 7;
-    medium = people / 3;
-    people = people % 3;
-    small = people;
-    area = (large) * (8 * 8) * (3.14159);
-    area = area + (medium) * (7 * 7) * (3.14159);
-    area = area + (small) * (5 * 5) * (3.14159);
-    area2 = area / people2;
-    cout << "For " << people2 << " people, area per person is: " << area2 << endl;
-
+    for (int people2 = 1; people2 <= 100; people2++) {
+        // Run the following code for 1 <= people2 <= 100
+        people = people2;
+        large = people / 7;
+        people = people % 7;
+        medium = people / 3;
+        people = people % 3;
+        small = people;
+        area = (large) * (8 * 8) * (3.14159);
+        area = area + (medium) * (7 * 7) * (3.14159);
+        area = area + (small) * (5 * 5) * (3.14159);
+        area2 = area / people2;
+        cout << "For " << people2 << " people, area per person is: " << area2 << endl;
+    }
 }
 
 void simpleForLoop() {
